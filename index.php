83c42f986b1644cf9d72a98c10644e8669369b64
Mercedes Arroyo <mercedes.arroyo@utelvt.edu.ec>
2022-11-18 19:56:44 -0500

    cambio de perfil

--- index.php
+++ index.php
@@ -80,14 +80,16 @@ include("marco-left.php")
  
 <script >  
 
- document.title="Mercedes"; 
-document.getElementById("foto").src="./images/stalin.png";
-document.getElementById("Mercedes Arroyo ").innerHTML="Mercedds Arroyo";
+document.title="Stalin";                                                   document.getElementById("foto").src="./images/stalin.png";                  document.getElementById("elnombre").innerHTML="Stalin Francis";
 
- document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
+ document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blo
+g-seccion4'>Hooby</a></p>";
 
- document.getElement("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href=''>Leonardo Nazareno</a></p><p><a href='eys-http:.php'>Maria Valverde</a></p> ";
+ document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacio
+nados:</b></p><hr> <p><a href='eys-kevin.php'>Kevin Aguilar</a></p><p><a hre
+f='eys-damaris.php'>Damaris Viallalva</a></p> ";
  </script>
+
  
 <?php
 include("foot.php")?>
